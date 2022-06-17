f59b41fdcc6d5e64589e21004dbb6c7b72e816b1
bobrs335 <106874866+bobrs335@users.noreply.github.com>
2022-06-17 15:53:14 +0300

    Add files via upload

---
+++ GLAVA.py
@@ -0,0 +1,34 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from dope import *
+from SECHAND import *
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(title_text)
+        self.resize(win_w,win_h)
+        self.move(win_x,win_y)
+    def initUI(self):
+        self.hello = QLabel(txt_hello)
+        self.instr = QLabel(txt_instr)
+        self.but = QPushButton(txt_but)
+        self.lay = QVBoxLayout()
+        self.lay.addWidget(self.hello,alignment = Qt.AlignCenter)
+        self.lay.addWidget(self.instr,alignment = Qt.AlignCenter)
+        self.lay.addWidget(self.but,alignment = Qt.AlignCenter)
+        self.setLayout(self.lay)
+    def connects(self):
+        self.but.clicked.connect(self.next_click)
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
---
+++ SECHAND.py
@@ -0,0 +1,33 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear():
+        self.setWindowTitle(title_text)
+        self.resize(win_w,win_h)
+        self.move(win_x,win_y)
+    def initUI():
+        self.tfio = QLabel(txt_tfio)
+        self.fio = QLineEdit(txt_fio)
+        self.tyo = QLabel(txt_tyo)
+        self.yo = QLineEdit(txt_yo)
+        self.instr1 = QLabel(txt_instr1)
+        self.but1 = QPushButton(txt_but1)
+        self.hearth1 = QLineEdit(txt_hearth1)
+        self.instr2 = QLabel(txt_instr2)
+        self.but2 = QPushButton(txt_but2)
+        self.instr3 = QLabel(txt_instr3)
+        self.but3 = QPushButton(txt_but3)
+        self.hearth11 = QLineEdit(txt_hearth11)
+        self.hearht12 = QlineEdit(txt_hearth12)
+        self.final_but = QPushButton(txt_final_but)
+        self.timer = QLabel(txt_timer)
+
+
+
